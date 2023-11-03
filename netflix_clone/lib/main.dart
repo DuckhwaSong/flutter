5b18d8f91aa3084a3af515978e06cf9d9e698d97
Duckhwa Song <kf80song@gmail.com>
2023-11-03 18:35:41 +0900

    플루터 스터디 2023-11-03

---
+++ netflix_clone/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/screen/home_screen.dart';
+import 'package:netflix_clone/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState()=>_MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //TabController controller;
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(title:'Nflix',
+    theme: ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.black, 
+      //colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.white).copyWith(secondary: Colors.grey),
+      ),
+    home: DefaultTabController(
+      length:4,
+      child:Scaffold(
+        body: TabBarView(
+          physics: NeverScrollableScrollPhysics(),
+          children: <Widget>[
+            //Container(child:Center(child:Text('홈'))),
+            HomeScreen(),
+            Container(child:Text('검색',style: TextStyle(fontSize:9))),
+            Container(child:Text('저장한컨텐츠목록',style: TextStyle(fontSize:9))),
+            Container(child:Text('더보기',style: TextStyle(fontSize:9))),
+          ],
+          ),
+          bottomNavigationBar: Bottom(),
+         ),
+      ),
+    );
+  }
+}
---
+++ netflix_clone/lib/screen/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  @override
+  void initState(){
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      child: Center(
+        child: Text('real home'),
+      ),
+    );
+  }
+}
+
