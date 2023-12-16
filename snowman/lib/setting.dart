215d41aef3f129f83ef6a3b4f7e817236df231d4
Duckhwa Song <kf80song@gmail.com>
2023-12-16 13:39:33 +0900

    설정 저장 개발중

--- snowman/lib/setting.dart
+++ snowman/lib/setting.dart
@@ -1,6 +1,90 @@
+import 'dart:async';  // for File
+import 'dart:io';     // for File
+import 'dart:math';   // for Random
+import 'dart:convert';   // for Json
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class CounterStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      final contents = await file.readAsString();
+
+      return int.parse(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$counter');
+  }
+}
+
+/*class ConfigStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
+  Future<int> readConfig() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      final contents = await file.readAsString();
+
+      return String.parse(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+
+  Future<File> writeConfig(int counter) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$counter');
+  }
+}*/
 
 class SettingPage extends StatelessWidget {
+
+  //final CounterStorage storage;
+  CounterStorage storage=new CounterStorage();
+  int testConter=0;
+  String JsonString = '{"name":"red"}';
+  Map<String, dynamic> color = jsonDecode('{"name":"red"}');
+
+  //print("$JsonString");
+
+
   @override
   Widget build(BuildContext context) {
     return ScaffoldMessenger(
@@ -67,6 +151,16 @@ class SettingPage extends StatelessWidget {
                       content: Text("설정이 저장되었습니다."),
                       duration: Duration(seconds: 3),
                     ));
+
+                    print("ID : ");
+
+                    testConter=Random().nextInt(6)+1;
+                    storage.writeCounter(testConter);
+                    print("저장하는 값:$testConter");
+                    storage.readCounter().then((value) {
+                      testConter = value;
+                      print("저장된 값:$testConter");
+                    });
                   },
                   child: Text('저장'),
                 ),
